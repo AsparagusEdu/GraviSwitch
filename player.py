a5f6a9ad922f81da04fd16165ca74b879b5a8ba9
AsparagusEdu <eduardo602002@gmail.com>
2014-11-11 20:16:15 -0300

    0.0.1.9
    - Ahora (player) es afectado por la gravedad, pero no colisiona aun.

--- player.py
+++ player.py
@@ -9,9 +9,15 @@ class Player(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.x = x_init + 8
 		self.rect.y = y_init
-	#def calc_grav():
+	def calc_grav(self):
+		if self.spd_y == 0:
+			self.spd_y = 1
+		else:
+			self.spd_y += .10
 		
 	def update(self):
+		self.calc_grav()
+		
 		self.rect.x += self.spd_x
 		self.rect.y += self.spd_y
 	
