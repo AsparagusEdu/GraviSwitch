9ca59335e7a5ffc4ef78c8c7c7c743256b4b0297
AsparagusEdu <eduardo602002@gmail.com>
2014-11-20 14:39:08 -0300

    0.4.3.57
    - La pantalla de titulo ahora tiene el link del github.
    - Eliminado codigo obsoleto.

--- main.py
+++ main.py
@@ -7,29 +7,18 @@ def main():
 	pygame.init()
 	pygame.display.set_caption(C.GAME_NAME)
 	Menu()
-	'''
-	done = False
-	while not done:
-		TitleScreen()
-		done = Level('levelTest1')
-		
-	done = False
-	while not done:
-		TitleScreen()
-		done = Level('level0')
-	'''
-	
-	#Level('level0')
-	#Level('Level2')
 main()
 
 
 #-------TO DO------
 '''
 - BUG: A veces el jugador se queda atascado al intentar subir un bloque.
-- BUG: Si un jugador cae de una caja a otra, cuenta como si muriera aplastado.
-	
-- Crear un switch y un sistema de "corriente"
 
+- BUG: Si un jugador cae de una caja a otra mientras ambas van subiendo, cuenta como si muriera aplastado.
+- BUG: Si el jugador esta saltando y choca con una caja que va cayendo hacia abajo, es aplastado
+Relacionados?
+
+- Crear un switch y un sistema de "corriente"
+- Crear Checkpoints
 
 '''
