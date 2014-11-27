bca4b7e13b1c0b45ef2b398bbae7beadcd06ec29
AsparagusEdu <eduardo602002@gmail.com>
2014-11-27 19:44:36 -0300

    0.5.3.95
    LIMPIEZA:
    - Como el antiguo formato de tiles de pared ya no es usado, ha sido
    removido. Los niveles que dependían del antiguo sistema han sido
    adaptados para funcionar con el nuevo.

--- code/Wall.py
+++ code/Wall.py
@@ -108,130 +108,74 @@ class Wall(pygame.sprite.Sprite):
 		return topleft, topright, bottomleft, bottomright
 	
 	def change_terrain(self, pared):
-		archivo = open('images/tiles/typesofsheets.txt', 'r')
-		tipo = 1
-		for linea in archivo:
-			if pared in linea.strip().split(':'):
-				break
-			tipo += 1
-		archivo.close()
+		
 		
 		sheet = pygame.image.load('images/tiles/' + pared).convert()
 		top, bottom, left, right = self.check_terrain_1()
 		
-		if tipo == 1:
-			if top:
-				if bottom:
-					if left:
-						if right:
-							cuadro = get_image(sheet, 0,0, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 32, 0, 32,32).convert()
-					else:
-						if right:
-							cuadro = get_image(sheet, 64, 0, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 96, 0, 32,32).convert()
-							
-				else:
-					if left:
-						if right:
-							cuadro = get_image(sheet, 0, 32, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 32, 32, 32,32).convert()
-					else:
-						if right:
-							cuadro = get_image(sheet, 64, 32, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 96, 32, 32,32).convert()
-			else:
-				if bottom:
-					if left:
-						if right:
-							cuadro = get_image(sheet, 0, 64, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 32, 64, 32,32).convert()
-					else:
-						if right:
-							cuadro = get_image(sheet, 64, 64, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 96, 64, 32,32).convert()
-				else:
-					if left:
-						if right:
-							cuadro = get_image(sheet, 0, 96, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 32, 96, 32,32).convert()
-					else:
-						if right:
-							cuadro = get_image(sheet, 64, 96, 32,32).convert()
-						else:
-							cuadro = get_image(sheet, 96, 96, 32,32).convert()
+		topleft, topright, bottomleft, bottomright = self.check_terrain_2(top, bottom, left, right)
+		cuadro = pygame.image.load('images/tiles/box.png').convert()
 		
-		elif tipo == 2:
-			topleft, topright, bottomleft, bottomright = self.check_terrain_2(top, bottom, left, right)
-			cuadro = pygame.image.load('images/tiles/box.png').convert()
-			
-			if top:
-				if left:
-					if topleft:
-						parte1 = get_image(sheet, 64,16, 16,16).convert()
-					else:
-						parte1 = get_image(sheet, 32,16, 16,16).convert()
+		if top:
+			if left:
+				if topleft:
+					parte1 = get_image(sheet, 64,16, 16,16).convert()
 				else:
-					parte1 = get_image(sheet, 16,16, 16,16).convert()
-				
-				if right:
-					if topright:
-						parte2 = get_image(sheet, 64,32, 16,16).convert()
-					else:
-						parte2 = get_image(sheet, 32,32, 16,16).convert()
-				else:
-					parte2 = get_image(sheet, 16,32, 16,16).convert()
-			
+					parte1 = get_image(sheet, 32,16, 16,16).convert()
 			else:
-				if left:
-					parte1 = get_image(sheet, 0,16, 16,16).convert()
-				else:
-					parte1 = get_image(sheet, 48,16, 16,16).convert()
-				
-				if right:
-					parte2 = get_image(sheet, 0,32, 16,16).convert()
-				else:
-					parte2 = get_image(sheet, 48,32, 16,16).convert()
-					
-			if bottom:
-				if left:
-					if bottomleft:
-						parte4 = get_image(sheet, 64,64, 16,16).convert()
-					else:
-						parte4 = get_image(sheet, 32,64, 16,16).convert()
-				else:
-					parte4 = get_image(sheet, 16,64, 16,16).convert()
-				
-				if right:
-					if bottomright:
-						parte3 = get_image(sheet, 64,48, 16,16).convert()
-					else:
-						parte3 = get_image(sheet, 32,48, 16,16).convert()
-				else:
-					parte3 = get_image(sheet, 16,48, 16,16).convert()
+				parte1 = get_image(sheet, 16,16, 16,16).convert()
 			
+			if right:
+				if topright:
+					parte2 = get_image(sheet, 64,32, 16,16).convert()
+				else:
+					parte2 = get_image(sheet, 32,32, 16,16).convert()
 			else:
-				if left:
-					parte4 = get_image(sheet, 0,64, 16,16).convert()
-				else:
-					parte4 = get_image(sheet, 48,64, 16,16).convert()
+				parte2 = get_image(sheet, 16,32, 16,16).convert()
+		
+		else:
+			if left:
+				parte1 = get_image(sheet, 0,16, 16,16).convert()
+			else:
+				parte1 = get_image(sheet, 48,16, 16,16).convert()
+			
+			if right:
+				parte2 = get_image(sheet, 0,32, 16,16).convert()
+			else:
+				parte2 = get_image(sheet, 48,32, 16,16).convert()
 				
-				if right:
-					parte3 = get_image(sheet, 0,48, 16,16).convert()
+		if bottom:
+			if left:
+				if bottomleft:
+					parte4 = get_image(sheet, 64,64, 16,16).convert()
 				else:
-					parte3 = get_image(sheet, 48,48, 16,16).convert()
-					
-			cuadro.blit(parte1, (0,0))
-			cuadro.blit(parte2, (16,0))
-			cuadro.blit(parte4, (0,16))
-			cuadro.blit(parte3, (16,16))
+					parte4 = get_image(sheet, 32,64, 16,16).convert()
+			else:
+				parte4 = get_image(sheet, 16,64, 16,16).convert()
+			
+			if right:
+				if bottomright:
+					parte3 = get_image(sheet, 64,48, 16,16).convert()
+				else:
+					parte3 = get_image(sheet, 32,48, 16,16).convert()
+			else:
+				parte3 = get_image(sheet, 16,48, 16,16).convert()
+		
+		else:
+			if left:
+				parte4 = get_image(sheet, 0,64, 16,16).convert()
+			else:
+				parte4 = get_image(sheet, 48,64, 16,16).convert()
+			
+			if right:
+				parte3 = get_image(sheet, 0,48, 16,16).convert()
+			else:
+				parte3 = get_image(sheet, 48,48, 16,16).convert()
+				
+		cuadro.blit(parte1, (0,0))
+		cuadro.blit(parte2, (16,0))
+		cuadro.blit(parte4, (0,16))
+		cuadro.blit(parte3, (16,16))
 		
 		#cuadro.set_colorkey(CHROMA_KEY)
 		
